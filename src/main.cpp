9e6b3d26fe5cdc6cdec1fabd7393757c0e72a51d
Daeun <yasmin34@naver.com>
2022-03-25 10:13:17 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <DHTesp.h>
+#include <Arduino.h>
+#include <SSD1306.h>
+
+SSD1306             display(0x3c, 4, 5, GEOMETRY_128_32);
+
+void readDHT22();
+
+DHTesp dht;
+int interval = 2000;
+unsigned long lastDHTReadMillis = 0;
+float humidity = 0;
+float temperature = 0;
+
+void setup() {
+  Serial.begin(115200);
+  dht.setup(14, DHTesp::DHT22);
+  delay(1000);
+
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_16);
+}
+
+void readDHT22(){
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - lastDHTReadMillis >= interval){
+    lastDHTReadMillis = currentMillis;
+
+    humidity = dht.getHumidity();
+    temperature = dht.getTemperature();
+  }
+}
+
+void loop() {
+  readDHT22();
+
+  display.clear();
+  display.drawString(0, 0, "Temp : " + String(temperature));
+  display.drawString(0, 15, "Humi : " + String(humidity));
+  display.display();
+
+  delay(1000);
+}
